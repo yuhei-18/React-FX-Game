9160fdc40342855c8638f3f355f5d2ac7d3bd75e
yuhei <kasaiyuhei8@gmail.com>
2021-09-11 20:24:10 +0900

    feat: Chakra UI インストール

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { ChakraProvider } from "@chakra-ui/react"
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './components/pages/Home'
+import Home from './components/pages/Home';
 
 class App extends React.Component {
   render() {
     return (
-      <>
+      <ChakraProvider>
         <Router>
           <Route exact path='/' component={Home} />
         </Router>
-      </>
+      </ChakraProvider>
     )
   }
 }
--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 class Home extends React.Component {
   render() {
     return (
-      <h1>Home Page !!!</h1>
+      <></>
     )
   }
 }
